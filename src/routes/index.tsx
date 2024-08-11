bdfee066d844b51c2f32f5319d150a2e879fb7f4
fmacedosantos <felipemacedo986@gmail.com>
2024-08-11 11:55:53 -0300

    Criando as navegações

---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
+
+import { Home } from '../screens/Home/Home'
+import { Forms } from '../screens/Forms/Forms'
+
+export function AppRoutes(){
+    return(
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Home'>
+                <Stack.Screen name='Home' component={Home}/>
+                <Stack.Screen name='Forms' component={Forms}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
