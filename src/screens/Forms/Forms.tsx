625179d4424a2d8450e86aa8f58e7ebe07940c64
fmacedosantos <felipemacedo986@gmail.com>
2024-08-11 12:54:56 -0300

    Estilizando o ícone

--- src/screens/Forms/Forms.tsx
+++ src/screens/Forms/Forms.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { View } from 'react-native';
 
 import { styles } from './Style';
+import { BatIcon } from '../../components/BatIcon/BatIcon';
+import { BatSmallIcon } from '../../components/BatSmallIcon/BatSmallIcon';
 
 export function Forms() {
   return (
     <View style={styles.container}>
-
+      <View style={styles.iconContainer}>
+        <BatSmallIcon></BatSmallIcon>
+      </View>
     </View>
   );
 }
--- src/screens/Forms/Style.tsx
+++ src/screens/Forms/Style.tsx
@@ -1,5 +1,19 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  iconContainer: {
+    flex:1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignSelf: 'flex-start',
+    marginHorizontal: 20,
+    marginTop: 30
+  }
+  
 });
